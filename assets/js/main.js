4c2c9901e0bdf698419d7d0b1cbf3fe38de7e6cc
maxirosanda <maxi_rosanda@hotmail.com>
2022-06-23 17:55:38 -0300

    kdsd

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+/*
+for(let i = 0 ; i < 10 ; i++){
+    console.log("el numero es :", i)
+}
+*/
+/*
+let numero = prompt("ingrese un numero")
+numero = parseInt(numero)
+for(let i = 1 ; i <= 10 ; i++){
+    let resultado = numero * i
+    console.log(numero + "X" + i + "=" + resultado)
+}
+*/
+/*
+for(let i = 1 ; i <= 20; i++){
+    let nombre = prompt("ingrese su nombre")
+    if(nombre == "maxi"){
+        break
+    }
+    console.log("Turno Numero: " + i + " Nombre: " + nombre)
+}
+*/
+/*
+for(let i = 0; i < 10 ; i++ ){
+    if(i == 5){
+        continue
+    }
+    console.log(i)
+}
+*/
+
+/*
+let valor = ["maxi","hernan","martin","matias","federico","belen"]
+console.log(valor.length)
+for(let i = 0 ; i < valor.length ; i++){
+    console.log(valor[i])
+}
+*/
+// el bucle no tiene salida (se traba el navegador)
+/*
+let valor = true
+
+while(valor){
+    console.log("al infito y... mas alla")
+}
+*/
+
+/*
+let entrada = prompt("ingrese una entrada")
+
+while(entrada != "esc"){
+    console.log(entrada)
+    entrada = prompt("ingrese una entrada")
+}
+*/
+/*
+let numero = 0
+
+do{
+    numero = prompt("ingrese un numero")
+    if(numero != "esc"){
+        alert(numero)
+    }else{
+        alert("no a ingresa ningun numero")
+    }
+    
+
+}while(numero != "esc")
+*/
+/*
+let numero = 0
+
+do{
+    numero = prompt("ingrese un numero")
+    console.log(numero)
+}while(parseInt(numero))
+console.log(parseInt(numero))
+*/
+/*
+
